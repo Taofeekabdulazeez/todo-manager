b116d91de9a7afc10e0ffe3cfb9ac3af09247815
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-03-12 23:12:54 +0100

    feat: make HTTP methods public in custom http client for improved accessibility

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,7 +1,7 @@
 import { ApiResponse } from "@/types";
 
 class Http {
-  async post<T>(url: string, data: object) {
+  public async post<T>(url: string, data: object) {
     const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(data),
@@ -14,21 +14,21 @@ class Http {
     return result as ApiResponse<T>;
   }
 
-  async get<T>(url: string) {
+  public async get<T>(url: string) {
     const response = await fetch(url, { method: "GET" });
     const result = await response.json();
 
     return result as ApiResponse<T>;
   }
 
-  async delete<T>(url: string) {
+  public async delete<T>(url: string) {
     const response = await fetch(url, { method: "DELETE" });
     const result = await response.json();
 
     return result as ApiResponse<T>;
   }
 
-  async patch<T>(url: string, data: object) {
+  public async patch<T>(url: string, data: object) {
     const response = await fetch(url, {
       method: "PATCH",
       body: JSON.stringify(data),
@@ -42,7 +42,7 @@ class Http {
     return result as ApiResponse<T>;
   }
 
-  async put<T>(url: string, data: object) {
+  public async put<T>(url: string, data: object) {
     const response = await fetch(url, {
       method: "PUT",
       body: JSON.stringify(data),
